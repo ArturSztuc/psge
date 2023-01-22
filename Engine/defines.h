eb0d0b88d2f39097f76e92cdab8a69b5c075010c
Artur Sztuc <a.sztuc@ucl.ac.uk>
2023-01-22 18:29:28 +0000

    Simple platform detection for future platform layer

--- Engine/defines.h
+++ Engine/defines.h
@@ -33,3 +33,35 @@ typedef String<128> S128;
 #define SETBIT(n, i) ((n) |= BIT(i))
 #define CLRBIT(n, i) ((n) &= ~BIT(i))
 #define TESTBIT(n, i) ((B8)(((n) & BIT(i)) != 0))
+
+// Platform detection
+// Windows
+#if defined(__WIN32__) || defined(_WIN32) || defined(WIN32)
+  #define PLATFORM_WINDOWS 1
+// MacOS
+#elif defined(__APPLE__)
+  #define PLATFORM_APPLE 1
+  #include <TargetConditionals.h>
+  // IPhone
+  #if TARGET_OS_IPHONE
+    #define PLATFORM_IPHONE 1
+    #error "IPhone platform not supported."
+  // IPad
+  #elif TARGET_OP_IOS
+    #define PLATFORM_IOS 1
+    #error "IPad platform not supported."
+  // MacOS
+  #elif TARGET_OS_OSX
+    #define PLATFORM_MAC
+  #else
+    #error "Apple platform, but cannot figure out which one"
+  #endif
+// Linux
+#elif defined(__linux__)
+  #define PLATFORM_LINUX 1
+// Android
+#elif defined(__ANDROID__) || defined(ANDROID)
+  #define PLATFORM_ANDROID 1
+#else
+  #error "Platform unknown!"
+#endif
